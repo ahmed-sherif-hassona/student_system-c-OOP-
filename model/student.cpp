37f91623ad0bb3edf33520f98c03406f3f9e8364
ahmed-sherif-hassona <ahmadshrif73@gmail.com>
2025-07-27 01:26:13 +0300

    create model baseEntity

--- model/student.cpp
+++ model/student.cpp
@@ -1,50 +1,12 @@
 
-#include<iostream>
-#include<string>
+#include "baseEntity.cpp"
 using namespace std;
-class Student{
+class Student : public baseEntity{
   private:
-      int age;
-       int id;
-       string name;
-       string phonenumber;
        double GPA;
 
  public:
-        void setAge(int a) {
-        age = a;
-    }
-
-    int getAge() const {
-        return age;
-    }
-
-  
-    void setId(int i) {
-        id = i;
-    }
-
-    int getId() const {
-        return id;
-    }
-
-    void setName(const string& n) {
-        name = n;
-    }
-
-    string getName() const {
-        return name;
-    }
-
-  
-    void setPhoneNumber(const string& p) {
-        phonenumber = p;
-    }
-
-    string getPhoneNumber() const {
-        return phonenumber;
-    }
-
+      
   
     void setGPA(double g) {
         GPA = g;
--- model/teacher.cpp
+++ model/teacher.cpp
@@ -1,48 +1,15 @@
-#include<iostream>
-#include<string>
+
+#include "baseEntity.cpp"
 using namespace std;
-class Teacher{
-    private:
-       int age;
-       int id;
-       string name;
-       string phonenumber;
+class Teacher : public baseEntity{
+
+
+ private:
+      
        double salary;
-       public:
-        void setAge(int a) {
-        age = a;
-    }
-
-    int getAge() const {
-        return age;
-    }
-
-  
-    void setId(int i) {
-        id = i;
-    }
-
-    int getId() const {
-        return id;
-    }
-
-    void setName(const string& n) {
-        name = n;
-    }
-
-    string getName() const {
-        return name;
-    }
-
-  
-    void setPhoneNumber(const string& p) {
-        phonenumber = p;
-    }
-
-    string getPhoneNumber() const {
-        return phonenumber;
-    }
-
+      
+ public:
+       
   
     void setSalary(double s) {
         salary = s;
