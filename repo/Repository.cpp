389e4810d2633bc310372d8fa731b2d38671c13f
ahmed-sherif-hassona <ahmadshrif73@gmail.com>
2025-07-29 17:54:56 +0300

    Repo(student-teacher-courses

---
+++ repo/Repository.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class studentRepository{
+
+};
+class studentRepositoryIMP :public studentRepository{
+
+};
+class courseRepository{
+
+};
+class courseRepositoryIMP :public courseRepository{
+
+};
+class teacherRepository{
+
+};
+class studentRepositoryIMP :public teacherRepository{
+
+};
